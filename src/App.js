e67d6e661c886a104f9dd1ca91eeee91f7f244da
rahul <rahul@example.com>
2025-04-18 16:18:42 +0530

    react boostarp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
+import axios from 'axios';
 
 function App() {
+  const title = "Reactivities"
+  const[activities,setActivies] = useState([])
+
+  useEffect(()=>{
+    const fetchActivities = async()=>{
+       try{
+           const response  = await  axios.get("https://localhost:5001/api/Activities");
+           if(!response.data || response.data.length===0){
+              console.log("no record found");
+              setActivies([])
+           }
+           else{
+           setActivies(response.data);
+           }
+       }
+       catch(error){
+        console.log(error);
+       }
+    }
+    fetchActivities();
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <h3 className='px-4 py-2'>{title}</h3>
+     <ListGroup >
+      {
+            activities.map((item)=>(
+             <ListGroup.Item key={item.id} style={{border:"none"}}>{item.title}</ListGroup.Item>
+            ))
+        }
+    </ListGroup>
     </div>
   );
 }
